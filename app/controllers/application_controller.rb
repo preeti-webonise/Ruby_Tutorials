6b94f3e04237040aa8f13ce45534b32db5aa79b6
Preeti Bhosale <preeti.bhosale@weboniselab.com>
2018-04-17 14:11:47 +0530

    first app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+	def hello
+		render html: "Hello, Welcome to rails" 
+	end
+
+	def bye
+		render html: "Good bye"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#hello' #root is the redirection and controller#action
+  get 'bye', to: 'application#bye' #get will be used ti get the rediection other than root
 end
