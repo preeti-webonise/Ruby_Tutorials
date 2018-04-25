aff8b83d06ef635a05fb7d823d552d0ca4923bbc
Preeti Bhosale <preeti.bhosale@weboniselab.com>
2018-04-25 11:47:22 +0530

    Complete signup page with all fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_24_101317) do
+ActiveRecord::Schema.define(version: 2018_04_24_143819) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.text "content"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2018_04_24_101317) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_hash"
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
