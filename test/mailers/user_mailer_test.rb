d23b6272de03f6814525dd88fddb763b67da54c1
Preeti Bhosale <preeti.bhosale@weboniselab.com>
2018-05-14 13:08:35 +0530

    Password reset functionality

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -12,4 +12,15 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match user.activationToken,   mail.body.encoded
     assert_match CGI.escape(user.email),  mail.body.encoded
   end
+
+  test "password_reset" do
+    user = users(:Shriya)
+    user.resetToken = User.newToken
+    mail = UserMailer.password_reset(user)
+    assert_equal "Password reset", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@sampleuserpostsapp.com"], mail.from
+    assert_match user.resetToken,        mail.body.encoded
+    assert_match CGI.escape(user.email),  mail.body.encoded
+  end
 end
