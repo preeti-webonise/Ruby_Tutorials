88cc26cce664f55072c73fff76dac84bcceb655d
Preeti Bhosale <preeti.bhosale@weboniselab.com>
2018-05-21 12:44:32 +0530

    removed the scaffold creation for posts

--- app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ApplicationRecord
-  validates :content, length: { maximum: 15 }, presence: true
-  validates :user_id, presence: true 
-  belongs_to :user
-end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   get 'help', to: 'static_pages#help'  
   # get 'static_pages/about'
   get 'about', to: 'static_pages#about'  
-  resources :posts
   resources :users
   root 'application#hello' 
   get  'signup', to: 'users#new'
